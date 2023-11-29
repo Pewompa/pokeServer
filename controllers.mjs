5e7ce623b8083f0efca66d9d641633c8a48a3c45
Pewompa <bernatpavon@gmail.com>
2023-11-29 14:53:38 +0100

    eeei

--- controllers.mjs
+++ controllers.mjs
@@ -1,16 +1,16 @@
 import pokemonModel from './schema.mjs';
 import indexModel from './indexSchema.mjs';
 
-async function getPokemon(req, res) {
-  try {
-    const { id } = req.params;
-    const data = await pokemonModel.findOne({ id: id });
-    res.status(200).json(data);
-  } catch (error) {
-    console.log('error fetching pokemon: ', error);
-    res.status = 400;
-  }
-}
+// async function getPokemon(req, res) {
+//   try {
+//     const { id } = req.params;
+//     const data = await pokemonModel.findOne({ id: id });
+//     res.status(200).json(data);
+//   } catch (error) {
+//     console.log('error fetching pokemon: ', error);
+//     res.status = 400;
+//   }
+// }
 
 // async function postNewPokemon(req, res) {
 //   try {
--- schema.mjs
+++ schema.mjs
@@ -1,19 +1,19 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 
-const pokemonSchema = new mongoose.Schema({
-  id: {
-    type: Number,
-    required: true,
-    unique: true,
-  },
-  name: {
-    type: String,
-    unique: true,
-  },
-  dateCalled: {
-    type: Date,
-    default: Date.now,
-  },
-});
-const pokemonModel = mongoose.model('pokemons', pokemonSchema);
-export default pokemonModel;
+// const pokemonSchema = new mongoose.Schema({
+//   id: {
+//     type: Number,
+//     required: true,
+//     unique: true,
+//   },
+//   name: {
+//     type: String,
+//     unique: true,
+//   },
+//   dateCalled: {
+//     type: Date,
+//     default: Date.now,
+//   },
+// });
+// const pokemonModel = mongoose.model('pokemons', pokemonSchema);
+// export default pokemonModel;
