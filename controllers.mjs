248b1c16cce471642ee6ce377e14e6d9b27fc6c5
Pewompa <bernatpavon@gmail.com>
2023-11-29 14:37:37 +0100

    eei

--- controllers.mjs
+++ controllers.mjs
@@ -12,18 +12,33 @@ async function getPokemon(req, res) {
   }
 }
 
+// async function postNewPokemon(req, res) {
+//   try {
+//     console.log('inside req.body', req.body);
+//     const pokemon = new indexModel({
+//       indexes: req.body.indexes,
+//     });
+//     await pokemon.save();
+//     res.status = 201;
+//     res.send(pokemon);
+//   } catch (error) {
+//     console.log('error posting pokemon: ', error)+
+//     res.status = 400;
+//   }
+// }
 async function postNewPokemon(req, res) {
   try {
     console.log('inside req.body', req.body);
-    const pokemon = new indexModel({
-      indexes: req.body.indexes,
-    });
-    await pokemon.save();
-    res.status = 201;
-    res.send(pokemon);
+    const query = { indexes: req.body.indexes };
+    const update = { indexes: req.body.indexes }; // You can update other fields here if needed
+    const options = { upsert: true, new: true, setDefaultsOnInsert: true };
+
+    const pokemon = await indexModel.findOneAndUpdate(query, update, options);
+
+    res.status(201).send(pokemon);
   } catch (error) {
     console.log('error posting pokemon: ', error);
-    res.status = 400;
+    res.status(400).send(error);
   }
 }
 
